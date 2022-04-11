99c435dff50320f4bace6855336b25772ebeb8fe
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-10 23:46:11 -0300

    employee entity

---
+++ src/main/java/com/github/pedroluiznogueira/testingapi/model/Employee.java
@@ -0,0 +1,28 @@
+package com.github.pedroluiznogueira.testingapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Employee {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "second_name", nullable = false)
+    private String secondName;
+
+    @Column(nullable = false)
+    private String email;
+}
